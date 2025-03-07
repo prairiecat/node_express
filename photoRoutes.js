c49d82ee36212e280a5980415a2440aa851ba15a
prairiecat <137054358+prairiecat@users.noreply.github.com>
2025-03-08 03:01:24 +0900

    photosの追加

--- photoRoutes.js
+++ photoRoutes.js
@@ -9,15 +9,30 @@ router.post("/photo", function(req, res) {
 
 router.post("/save-photo", function(req, res) {
     const photoData = req.body.photo;
-    // 写真データを保存する処理を追加
     const base64Data = photoData.replace(/^data:image\/png;base64,/, "");
-    fs.writeFile("saved_photo.png", base64Data, 'base64', function(err) {
+    const fileName = `${Date.now()}.png`;
+    const filePath = path.join(__dirname, 'photos', fileName);
+
+    fs.writeFile(filePath, base64Data, 'base64', function(err) {
         if (err) {
             console.log(err);
             res.status(500).send("写真の保存に失敗しました");
         } else {
-            // 写真が保存された後の処理を追加
             res.sendFile(path.join(__dirname, 'photosended.html'));
+        }
+    });
+});
+
+// 写真を表示するエンドポイント
+router.get("/photos/:fileName", function(req, res) {
+    const fileName = req.params.fileName;
+    const filePath = path.join(__dirname, 'photos', fileName);
+
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            res.status(404).send("写真が見つかりません");
+        } else {
+            res.sendFile(filePath);
         }
     });
 });
